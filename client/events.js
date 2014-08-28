3687af9eac81a314d3daa25f7f55f3a01630cedd
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-08-28 17:32:48 +0530

    over simplified the app

--- client/events.js
+++ client/events.js
@@ -1,51 +1,18 @@
 Template.manager.events({
-  "click #publish": function() {
-    var strPackages = $('#package-store').val();
-    var lines = strPackages.split('\n').map(doTrim);
-    var packages = [];
-    for(var lc = 0; lc< lines.length; lc++) {
-      var line = lines[lc];
-      var parts = line.split(",").map(doTrim);
-      var name = parts[0];
-      var version = parts[1];
-
-      try {
-        var deps = (parts[2])? getObj(parts[2]) : {};
-      } catch(ex) {
-        return SetMessage("error when parsing: " + parts[2] + "\n> " + ex.message);
-      }
-
-      packages.push([name, version, deps]);
-    }
-
-    var name = GetName();
-    Meteor.call('publishPackages', name, packages, function(err) {
-      if(err) {
-        SetMessage(err.message);
-      } else {
-        UpdateUrl({packages: strPackages});
-        SetMessage('packges published!');
-      }
-    });
-  },
-
   "click #resolve": function() {
+    
     var strDeps = $('#application-deps').val();
-    var lines = strDeps.split("\n").map(doTrim);
-    var deps = {};
+    var deps = getDeps(strDeps);
 
-    for(var lc = 0; lc< lines.length; lc++) {
-      var line = lines[lc];
-      var parts = line.split('@').map(doTrim);
-      deps[parts[0]] = parts[1] || "";
-    }
+    var strPackages = $('#package-store').val();
+    var packages = getPackages(strPackages);
 
-    var name = GetName();
-    Meteor.call('resolve', name, deps, function(err, res) {
+    SetMessage("resolving...", true);
+    Meteor.call('resolve', packages, deps, function(err, res) {
       if(err) {
         SetMessage(err.message);
       } else if(res.resolvedDeps){
-        UpdateUrl({deps: strDeps});
+        UpdateUrl({deps: strDeps, packages: strPackages});
         SetMessage(JSON.stringify(res.resolvedDeps, null, 2), true);
       } else if(res.error) {
         SetMessage("ERROR: " + res.error, true);
@@ -53,6 +20,40 @@ Template.manager.events({
     });
   }
 });
+
+function getPackages (strPackages) {
+  var lines = strPackages.split('\n').map(doTrim);
+  var packages = [];
+  for(var lc = 0; lc< lines.length; lc++) {
+    var line = lines[lc];
+    var parts = line.split(",").map(doTrim);
+    var name = parts[0];
+    var version = parts[1];
+
+    try {
+      var deps = (parts[2])? getObj(parts[2]) : {};
+    } catch(ex) {
+      return SetMessage("error when parsing: " + parts[2] + "\n> " + ex.message);
+    }
+
+    packages.push([name, version, deps]);
+  }
+
+  return packages;
+}
+
+function getDeps (strDeps) {
+  var lines = strDeps.split("\n").map(doTrim);
+  var deps = {};
+
+  for(var lc = 0; lc< lines.length; lc++) {
+    var line = lines[lc];
+    var parts = line.split('@').map(doTrim);
+    deps[parts[0]] = parts[1] || "";
+  }
+
+  return deps;
+}
 
 function getObj(str) {
   var obj;
--- server/app.js
+++ server/app.js
@@ -1,37 +1,10 @@
-var packageManagers = {};
-var subscribedNames = {};
-
-Meteor.publish('packageManager', function(name) {
-  if(subscribedNames[name]) {
-    throw new Meteor.Error(403, "existing package manager: " + name + " - create a new one");
-  }
-
-  subscribedNames[name] = true;
-  this.ready();
-  this.onStop(function() {
-    var pm = packageManagers[name];
-    if(pm) {
-      packageManagers[name] = null;
-      pm.cleanup();
-    }
-    subscribedNames[name] = null;
-  });
-});
-
 Meteor.methods({
-  publishPackages: function(name, packageList) {
-    checkForname(name);
-    var pm = new PackageManager(name);
-    packageManagers[name] = pm;
-    
+  resolve: function(packageList, deps) {
+    var pm = new PackageManager();
     packageList.forEach(function(args) {
       pm.publishPackage.apply(pm, args);
     });
-  },
 
-  resolve: function(name, deps) {
-    checkForname(name);
-    var pm = packageManagers[name];
     try { 
       return {
         resolvedDeps: pm.resolve(deps)
@@ -43,11 +16,3 @@ Meteor.methods({
     }
   }
 });
-
-
-function checkForname(name) {
-  var exists = subscribedNames[name];
-  if(!exists) {
-    throw new Meteor.Error(403, "please wait while loading for: " + name);
-  }
-}
